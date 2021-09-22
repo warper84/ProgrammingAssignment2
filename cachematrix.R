5acfa567111bf0039b14f157ccbda6e1a67f12f5
Michal Zaruba <michal.zaruba@thermofisher.com>
2021-09-22 12:41:03 +0200

    Functions 'makeCacheMatrix', and 'cacheSolve' implemented.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function makeCacheMatrix "wraps" its argument, a matrix, into a list of
+## "interface" functions (like methods of a class). This list is intended to be
+## consumed by the function cacheSolve, where these "interface" functions are
+## referenced and enable neat manipulation with this "wrapped" matrix in order
+## to compute the inverse to it (or return the cached value from the previous
+## calculation provided the original matrix has not changed).
 
-## Write a short comment describing this function
+## Function makeCacheMatrix expects a matrix as an argument and returns a list
+## of accessor functions (methods) to this matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverseMatrix <- NULL
+	set <- function(newX) {
+		x <<- newX
+		inverseMatrix <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inverse) inverseMatrix <<- inverse
+	getInverse <- function() inverseMatrix
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve expects a list of accessor functions (methods),
+## to a matrix, i.e. an output from the function makeCacheMatrix, and
+## it returns an invere to the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getInverse()
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setInverse(inv)
+	inv
 }
